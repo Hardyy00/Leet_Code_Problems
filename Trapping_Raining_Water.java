2159adb232b91330dd6c4cb362f4a7be78718290
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2022-09-24 23:43:11 +0530

    Add files via upload

---
+++ Trapping_Raining_Water.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int trap(int[] height) {
+
+        return trappingWater(height);
+
+    }
+
+    private static int trappingWater(int[] arr) {
+
+        int[] left = new int[arr.length];
+        left[0] = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+
+            left[i] = Math.max(left[i - 1], arr[i]);
+        }
+
+        int[] right = new int[arr.length];
+
+        right[arr.length - 1] = arr[arr.length - 1];
+
+        for (int i = arr.length - 2; i >= 0; i--) {
+
+            right[i] = Math.max(arr[i], right[i + 1]);
+        }
+
+        int water = 0;
+        for (int i = 0; i < arr.length; i++) {
+
+            water += Math.min(left[i], right[i]) - arr[i];
+        }
+
+        return water;
+    }
+}
