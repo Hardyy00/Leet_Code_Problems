3a8883ce66c7c6d2d001b58f9168828cc4ffa84d
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2023-04-04 08:58:50 +0530

    Time: 17 ms (98.32%), Space: 50.7 MB (60.52%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        
+        Arrays.sort(people);
+    
+        int boats = 0;
+        int i=0;
+        int j=people.length-1;
+        
+            
+        while(i<=j){
+            
+            if(people[j]+people[i]>limit){
+                j--;
+            }else{
+                i++;
+                j--;
+            }
+            
+            boats++;
+        }
+        
+        return boats;
+        
+    }
+}
