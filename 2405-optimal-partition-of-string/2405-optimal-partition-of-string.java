766520052da6b0ec7ff2f661b398bc968a0ec50d
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2023-04-04 21:46:17 +0530

    Time: 11 ms (85.92%), Space: 43.4 MB (45.13%) - LeetHub

--- 2405-optimal-partition-of-string/2405-optimal-partition-of-string.java
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.java
@@ -1,22 +1,34 @@
 class Solution {
     public int partitionString(String s) {
         
+        int in = 0;
+        int si = 0;
         int count = 0;
-        Set<Character> set = new HashSet<>();
+        int[] fre = new int[26];
         
         for(int i=0;i<s.length();i++){
             
             char ch = s.charAt(i);
             
-            if(set.contains(ch)){
-                set.clear();
-                count++;
+            fre[ch-97]++;
+            
+            // if a character appears more than 1 times then start the new substring from the current 
+            // index
+            if(fre[ch-97]>1){
+                
+                while(si<in){
+                    fre[s.charAt(si)-97]--;
+                    si++;
+                }
             }
             
-            set.add(ch);
+            if(si==in){
+                count++;
+
+            }
+            
+            in++;
         }
-        
-        count++;
         
         return count;
         
