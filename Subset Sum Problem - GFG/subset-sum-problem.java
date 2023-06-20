7ba6195440703e891865b377977a96140d1ebe3a
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2023-06-20 19:05:35 +0530

    Added solution - LeetHub

--- Subset Sum Problem - GFG/subset-sum-problem.java
+++ Subset Sum Problem - GFG/subset-sum-problem.java
@@ -38,14 +38,47 @@ class Solution{
 
     static Boolean isSubsetSum(int n, int arr[], int sum){
         
-        int[][] dp = new int[n][sum+1];
+        // int[][] dp = new int[n][sum+1];
         
-        for(int[] row : dp) Arrays.fill(row,-1);
+        // for(int[] row : dp) Arrays.fill(row,-1);
         
-        return findTargetSubset1(n-1,sum,arr,dp)==1;
+        // return findTargetSubset1(n-1,sum,arr,dp)==1;
+        
+        return findTargetSubset2(n,sum,arr);
+        
+    }
+    
+    static boolean findTargetSubset2(int n, int sum, int[] arr){
+        
+        boolean[][] dp = new boolean[n][sum+1];
+        
+        for(int i=0;i<n;i++) dp[i][0] =true;
+        
+        if(arr[0]<=sum)
+            dp[0][arr[0]] = true;
+        
+        
+        for(int i=1;i<n;i++){
+            
+            for(int target=1;target<=sum;target++){
+                
+                boolean pick = false;
+                if(arr[i]<=target){
+                    pick = dp[i-1][target-arr[i]];
+                }
+                
+                boolean notPick = dp[i-1][target];
+                
+                dp[i][target] = pick || notPick;
+            }
+        }
+        
+        return dp[n-1][sum];
     }
     
     static int findTargetSubset1(int index, int target, int[] arr, int[][] dp){
+        
+        // TC : O(N*target) && SC : O(N*target) + O(N)
         
         if(target==0) return 1;
         if(dp[index][target]!=-1) return dp[index][target];
