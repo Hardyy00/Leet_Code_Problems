32f756c94b9fc799c10fb2eccc2233f480cce2c9
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2023-06-27 22:37:02 +0530

    Added solution - LeetHub

--- Rod Cutting - GFG/rod-cutting.java
+++ Rod Cutting - GFG/rod-cutting.java
@@ -28,13 +28,54 @@ class RodCutting {
 class Solution{
     public int cutRod(int price[], int n) {
         
-        int[][] dp = new int[n][n+1];
-        for(int[] row : dp) Arrays.fill(row,-1);
+        // int[][] dp = new int[n][n+1];
+        // for(int[] row : dp) Arrays.fill(row,-1);
         
-        return cutTheRod1(n-1,n,price,dp);
+        // return cutTheRod1(n-1,n,price,dp);
+        
+         return cutTheRod2(n,price);
+        
+        
+    }
+    
+     static int cutTheRod2(int n, int[] prices){
+         
+         // Tabulation
+         // TC : O(N*N) 
+         // SC : O(N*N)
+
+        int[][] dp = new int[n][n+1];
+
+        for(int i=0;i<=n;i++){
+            dp[0][i] = i*prices[0];
+        }
+
+        for(int i=1;i<n;i++){
+
+            for(int len= 0; len<=n;len++){
+
+                int notCut = dp[i-1][len];
+
+                int cut = 0;
+
+                if(i+1<=len){
+                    cut = prices[i] + dp[i][len-(i+1)];
+                }
+
+                dp[i][len] = Math.max(cut,notCut);
+
+            }
+        }
+
+        return dp[n-1][n];
     }
     
     static int cutTheRod1(int index, int rodLength,int[] prices, int[][] dp){
+        
+        
+        // Memoization
+        // TC : O(N*N)
+        // sc : O(N*N) + O(N)
 
 
        if(dp[index][rodLength]!=-1) return dp[index][rodLength];
