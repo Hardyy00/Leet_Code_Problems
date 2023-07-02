2e6f270a16b04e817dee74bed55955e073453378
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2023-07-02 23:28:18 +0530

    Added solution - LeetHub

--- Minimum number of deletions and insertions. - GFG/minimum-number-of-deletions-and-insertions..cpp
+++ Minimum number of deletions and insertions. - GFG/minimum-number-of-deletions-and-insertions..cpp
@@ -14,11 +14,31 @@ class Solution{
 	   
 	   vector<vector<int>> dp(n , vector<int>(m , -1));
 	   
-	   int maxLCS =  lcs(n-1,m-1,str1,str2,dp);
+	   //int maxLCS =  lcs(n-1,m-1,str1,str2,dp);
+	   
+	   //return n+m -(2* maxLCS);
+	   
+	   int maxLCS =  lcs2(n,m,str1,str2);
 	   
 	   return n+m -(2* maxLCS);
 	    
 	} 
+	
+	int lcs2(int n, int m, string &s1, string &s2){
+	    
+	    vector<vector<int>> dp(n+1, vector<int>(m+1 , 0));
+	    
+	    for(int i=1;i<=n;i++){
+	        
+	        for(int j=1;j<=m;j++){
+	            
+	            if(s1[i-1]==s2[j-1]) dp[i][j] = 1 + dp[i-1][j-1];
+	            else dp[i][j] = max( dp[i-1][j] , dp[i][j-1]);
+	        }
+	    }
+	    
+	    return dp[n][m];
+	}
 	
 	int lcs(int index1, int index2, string &s1, string &s2, vector<vector<int>> &dp){
 	    
