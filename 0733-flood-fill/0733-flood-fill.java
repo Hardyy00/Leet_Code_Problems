d6cd8d999903113dc71d11d24fbb92e2083e8d72
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2023-07-25 23:22:34 +0530

    Time: 1 ms (49.21%), Space: 44.6 MB (5.25%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.java
@@ -0,0 +1,89 @@
+class Solution {
+    static int[] ra = {-1,0,1,0};
+    static int[] ca = {0,1,0,-1};
+
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+
+
+        int m = image.length;
+        int n = image[0].length;
+
+        int[][] ans = new int[m][n];
+        
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++) ans[i][j] = image[i][j];
+        }
+
+        dfs(sr,sc,color,ans,image, image[sr][sc]);
+
+        // bfs(sr,sc,color,ans,image);
+
+        return ans;
+    }
+
+    private void bfs(int sr, int sc, int color,int[][] ans, int[][] image){
+
+        // TC : O(M*N)
+        // SC : O(M*N) + O(M*N) (for ans matrix + queue nearly at max may contains m*n nodes )
+
+        int m = image.length;
+        int n = image[0].length;
+
+        // queue contains the nearest (row,column) cells
+        Queue<Pair<Integer,Integer>> queue = new LinkedList<>();
+
+        queue.offer(new Pair(sr,sc));
+
+        int matchColor = image[sr][sc];     // storing the color for which we will visit cells
+
+        while(!queue.isEmpty()){
+
+            int r = queue.peek().getKey();
+            int c = queue.peek().getValue();
+
+            ans[r][c] = color;
+            queue.poll();
+
+            for(int i=0;i<4;i++){
+
+                int nr = r + ra[i];
+                int nc = c + ca[i];
+
+                // visit only those cell which has same color as the starting cell and are not already colored
+                if(nr>=0 && nc>=0 && nr < m && nc < n && image[nr][nc]==matchColor && ans[nr][nc]!=color){
+                    // since we inserted them in the queue, we will  definiitely visit them , so mark
+                    // them true   
+                    queue.offer(new Pair(nr,nc));
+                }
+            }
+        }
+
+    }
+
+    private void dfs(int r, int c, int color,int[][] ans,int[][] image, int matchColor){
+
+        // TC : O(M*N) + O(M*N *4)       (N^2 for the outer loop  + N^2 * 4 for the overall dfs as we are overall visiting all)
+        // the nodes with one
+        
+        // SC : O(M*N) (for ans matrix) + O(M*N) (approx recusion stack in case of full 1 matrix)
+        
+        // basically , we are calculting no of components, such that applying dfs to one component does not reach another
+
+        int m = image.length;
+        int n = image[0].length;
+
+        ans[r][c] = color;
+
+        for(int i=0;i<4;i++){
+
+            int nr = r + ra[i];
+            int nc = c + ca[i];
+
+            // visit only those cell which has same color as the starting cell and are not already colored
+            if(nr>=0 && nr<m && nc>=0 && nc<n && image[nr][nc]==matchColor && ans[nr][nc]!=color){
+
+                dfs(nr,nc,color,ans,image, matchColor);
+            }
+        }
+    }
+}
