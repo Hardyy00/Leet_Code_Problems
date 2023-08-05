a31c77e4525b4ef176d1cf40c06c99d4421d5e1f
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2023-08-05 23:07:44 +0530

    Time: 13 ms (56.82%), Space: 49 MB (82.22%) - LeetHub

--- 2090-number-of-ways-to-arrive-at-destination/2090-number-of-ways-to-arrive-at-destination.java
+++ 2090-number-of-ways-to-arrive-at-destination/2090-number-of-ways-to-arrive-at-destination.java
@@ -15,34 +15,37 @@ class Solution {
     int mod = (int)1e9 + 7;
     public int countPaths(int n, int[][] roads) {
 
-        int e = roads.length;
+        // using dijkstra's algo, to find the shortest path, and maintaining number of ways
+        // to reach a node
+        // TC : O(E*Log V) == O(N * Log E) (where e is roads.length)
         List<List<Pair>> adj = new ArrayList<>();
 
         for(int i=0;i<n;i++) adj.add(new ArrayList<>());
 
         for(int[] it : roads){
 
+            // nodes are bi-directional
             adj.get(it[0]).add(new Pair(it[1], it[2]));
             adj.get(it[1]).add(new Pair(it[0], it[2]));
         }
 
-        long[] dis = new long[n];
-        int[] ways = new int[n];
+        long[] dis = new long[n];   // time <= 10^9 , so take a long array
+        int[] ways = new int[n];    // to count numbers of ways to each node
         PriorityQueue<Pair> pq = new PriorityQueue<>(new Comparator<Pair>(){
 
             @Override
             public int compare(Pair p1, Pair p2){
 
-                return p1.dis < p2.dis ? -1 : 1;
+                return p1.dis < p2.dis ? -1 : 1;    // sort acc. to dis
             }
         });
 
-        Arrays.fill(dis, (long)1e12);
+        Arrays.fill(dis, (long)1e12);       //10^9 * 200 == 10^11, hence take infinity == 10^12
 
-        dis[0] = 0;
-        ways[0] = 1;
+        dis[0] = 0; // self distance is 0
+        ways[0] = 1;    // ways to reach self is 0
 
-        pq.offer(new Pair(0,0));
+        pq.offer(new Pair(0,0));    // node + dis
 
         while(!pq.isEmpty()){
 
@@ -56,26 +59,28 @@ class Solution {
                 int adjNode = pair.node;
                 long d = pair.dis;
 
+                // change on finding a better distance
                 if(time + d< dis[adjNode]){
 
                     dis[adjNode] = (time+d);
-
-                    ways[adjNode] = ways[node]%mod;
+                    
+                    // on finding a better distance , discard old ways and replace them
+                    // ways number of ways to reach previous ways,as in those ways we
+                    // can this node
+                    ways[adjNode] = ways[node]%mod; 
 
                     pq.offer(new Pair(adjNode,dis[adjNode]));
 
                 }else if(time + d == dis[adjNode]){
-
+                    
+                    // if the distance are same , just add the ways of 'node' , to adjNode ways
                     ways[adjNode] += ways[node]%mod;
 
-                    ways[adjNode] %= mod;
+                    ways[adjNode] %= mod;   // take mod
 
                 }
             }
         }
-
-        // System.out.println(Arrays.toString(dis));
-        // System.out.println(Arrays.toString(ways));
 
         return ways[n-1]%mod;
 
