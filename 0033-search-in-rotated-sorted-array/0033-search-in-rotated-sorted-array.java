2a0f4aa93fd7e147377d59cf4c5b0b8448d302fc
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2023-08-08 05:38:01 +0530

    Time: 0 ms (100%), Space: 40.5 MB (96.91%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -10,7 +10,12 @@ class Solution {
             int mid = high + (low - high)/2;
             
             if(nums[mid]==target) return mid;
+            
+            // check if left side is sorted
             else if(nums[low]<=nums[mid]){
+                
+                // checking if the element exist within the left side
+                // if it exist then shifting the search space to left else to right
                 if(target>=nums[low] && target<=nums[mid]){
                     high = mid-1;
                 }else{
@@ -18,10 +23,14 @@ class Solution {
                 }
             }else{
                 
-                if(target>=nums[low] || target<=nums[mid]){
-                    high = mid-1;
+                // for example like 6 7 1 2 3 4 5 . for finding 1 , 1<=2 || 1>=6 (number might be greater than 6)
+                // if(target>=nums[low] || target<=nums[mid])
+                
+                // checking if the element exist on the sorted right side
+                if(target>=nums[mid] && target<=nums[high]){
+                    low = mid+1;
                 }else{
-                    low = mid + 1;
+                    high = mid - 1;
                 }
             }
             
