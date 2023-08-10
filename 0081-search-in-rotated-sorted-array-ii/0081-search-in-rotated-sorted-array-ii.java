64324c0c72b6b68e681314a042b25a5a67ce0e7a
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2023-08-11 00:12:23 +0530

    Time: 0 ms (100%), Space: 42.8 MB (91.85%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -1,6 +1,60 @@
 class Solution {
     public boolean search(int[] nums, int target) {
+
+        // return solve1(nums, target);
+
+        return solve2(nums, target);
         
+        
+    }
+
+    private boolean solve2(int[] nums, int target){
+
+        int low = 0;
+        int high = nums.length-1;
+
+        while(low<=high){
+
+            int mid = high + (low - high)/2;
+
+            if(nums[mid]== target) return true;
+
+            if(nums[mid]==nums[low] && nums[mid]==nums[high]){
+                low++;
+                high--;
+                continue;
+            }
+
+            if(nums[low]<=nums[mid]){
+
+                if(target>=nums[low] && target<=nums[mid]){
+
+                    high = mid -1;
+                }else{
+
+                    low = mid +1;
+                }
+            }else if(nums[mid]<=nums[high]){
+
+                if(target>=nums[mid] && target<=nums[high]){
+
+                   low = mid+1;
+                }else{
+
+                   high = mid-1;
+                }
+            }
+        }
+
+        return false;
+        
+    }
+
+    private boolean solve1(int[] nums , int target){
+
+        // Tc : O(N*Log N) + Log N
+        // Sc : O(1)
+
         Arrays.sort(nums);
 
         int low = 0;
