a4b04e687db75b0d77b7e7a94943ef2e3048b347
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2023-08-13 06:50:04 +0530

    Time: 122 ms (92.31%), Space: 95.4 MB (8%) - LeetHub

---
+++ 2443-check-if-there-is-a-valid-partition-for-the-array/2443-check-if-there-is-a-valid-partition-for-the-array.cpp
@@ -0,0 +1,81 @@
+class Solution {
+private:
+    bool solve(int index, vector<int> &nums){
+
+        if(index==nums.size()) return true;
+
+        int same = 0;
+        int cons = 0;
+        bool ans = false;
+
+        for(int i=index;i<index+3 && i<nums.size();i++){
+
+            if(i==index){
+                same=cons=1;
+            }else if(nums[i]==nums[i-1]){
+                same++;
+                
+            }else if(nums[i]-nums[i-1]==1) cons++;
+
+
+            if(same>=2 || cons==3){
+                cout << "Part at " << i << endl;
+                ans = ans || solve(i+1, nums);
+            }
+ 
+        }
+
+        return ans;
+    }
+
+    bool solve2(int index, vector<int> &nums, vector<int> &dp){
+
+        if(index==nums.size()) return true;
+        if(dp[index]!=-1) return dp[index]; 
+
+        int same = 0;
+        int cons = 0;
+        bool ans = false;
+        bool calledFor2 = false;
+
+        for(int i=index;i<index+3 && i<nums.size();i++){
+
+            if(i==index){
+                same=cons=1;
+            }else if(nums[i]==nums[i-1]){
+                same++;
+                
+            }else if(nums[i]-nums[i-1]==1){ 
+                cons++;
+            }
+
+            else{
+                return dp[index]=ans;
+            }
+
+            if((same==2 && cons==1) && !calledFor2){
+                calledFor2 = true;
+                ans = ans || solve2(i+1, nums,dp);
+
+            }else if(same==3|| cons==3){
+                // cout << "Index : " << index  << " Part at " << i << endl;
+                ans = ans || solve2(i+1, nums,dp);
+            }
+ 
+        }
+
+        return dp[index]=ans;
+    }
+public:
+    bool validPartition(vector<int>& nums) {
+
+        if(nums.size()==1 || (nums.size()==2 && nums[0]!=nums[1])) return false;
+        
+        int n = nums.size();
+        vector<int> dp(n,-1);
+
+        return solve2(0,nums,dp);
+
+    }
+
+};
