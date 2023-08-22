b76023a766f346e7c5295b57ff987cd89acad1e3
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2023-08-22 06:11:55 +0530

    Time: 13 ms (98.11%), Space: 48.6 MB (25.53%) - LeetHub

--- 1558-course-schedule-iv/1558-course-schedule-iv.java
+++ 1558-course-schedule-iv/1558-course-schedule-iv.java
@@ -1,6 +1,13 @@
 class Solution {
     public List<Boolean> checkIfPrerequisite(int numCourses, int[][] pre, int[][] queries) {
         
+        // TC : O(N^3) 
+        // SC : O(N*N) + O(N)
+
+        // if mat[a][b] = 1 , i.e a is pre of b, then visit,b and see it is connected to which node
+        // when you complete the call return the array, so that the b can know for who
+        // c was pre, b would also be a pre of them, and similarily return mat[b]
+        // so that a can mark for whom b (mixed with c)  was a pre/
 
         if(pre.length == 0){
 
@@ -20,6 +27,7 @@ class Solution {
 
         boolean[] visit = new boolean[numCourses];
 
+        // call for dfs
         for(int i=0;i<numCourses;i++){
 
             if(!visit[i]) dfs(i, mat,visit, numCourses);
@@ -38,31 +46,35 @@ class Solution {
 
     private boolean[]  dfs(int node, boolean[][] mat,boolean[] visit,int n ){
 
-        visit[node] = true;
+        visit[node] = true;  // visit the node
 
-        boolean[] save = new boolean[n];
+        boolean[] save = new boolean[n];  // to save answers for every call and , appling them in the end
 
         for(int i=0;i<n;i++){
 
+            // if a is pre of b, then visit b
             if(mat[node][i]){
 
                 boolean[] temp = null;
-                if(visit[i]){
+                if(visit[i]){  // is b is already visited , then get b , cuz we want to mark , for whom b was a pre
 
                     temp = mat[i];
 
                 }else{
-
+                    
+                    // other wise call for dfs
                     temp = dfs(i, mat, visit, n);
                 }
 
+                // save the answerrs
                 for(int j=0;j<n;j++) save[j] = save[j] || temp[j];
             }
         }
 
+        // apply the answers
         for(int i=0;i<n;i++) mat[node][i] = mat[node][i] || save[i];
 
-        return mat[node];
+        return mat[node];  // return the info about the current node
 
     }
 
