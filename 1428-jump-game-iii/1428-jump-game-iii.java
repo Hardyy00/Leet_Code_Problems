1d32bc0b4407d5113c29f26eb349f96403b726df
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2023-08-22 09:18:52 +0530

    Time: 1 ms (98.85%), Space: 57.2 MB (46%) - LeetHub

--- 1428-jump-game-iii/1428-jump-game-iii.java
+++ 1428-jump-game-iii/1428-jump-game-iii.java
@@ -1,9 +1,43 @@
 class Solution {
     public boolean canReach(int[] arr, int start) {
         
-       return solve1(arr,start);
+    //    return solve1(arr,start);
+
+        return solve2(arr, start);
     }
 
+    private boolean solve2(int[] arr, int start){
+
+        int n = arr.length;
+        boolean[] visit = new boolean[n];
+
+        return dfs(start, arr, visit);
+    }
+
+    private boolean dfs(int index, int[] arr, boolean[] visit){
+
+        if(arr[index]==0) return true;
+
+        visit[index] = true;
+        int n = arr.length;
+
+        int plus = index + arr[index];
+
+        if(plus >=0 && plus< n && !visit[plus]){
+
+            if( dfs(plus, arr, visit)) return true;
+
+        } 
+
+        int minus = index - arr[index];
+
+        if( minus >=0 && minus < n && !visit[minus] ) {
+
+            if( dfs(minus, arr, visit) ) return true;
+        }
+
+        return false;
+    }
     private boolean solve1(int[] arr, int start){
 
         // Applying bfs
@@ -22,8 +56,10 @@ class Solution {
 
             int plus = curr + arr[curr];
 
+            // if the index is within the boundary and is not visited visit it
             if(plus>=0 && plus< n && !visit[plus]){
 
+                // at this index zero exist , so return true
                 if(arr[plus] == 0) return true;
 
                 q.offer(plus);
@@ -41,6 +77,6 @@ class Solution {
             }
         }
 
-        return false;
+        return false;  
     }
 }
