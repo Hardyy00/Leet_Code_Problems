2323955072670c003a5aa24cdea0aebd7cafb2e2
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2023-08-22 09:12:34 +0530

    Time: 8 ms (39.65%), Space: 52.9 MB (96.16%) - LeetHub

--- 1428-jump-game-iii/1428-jump-game-iii.java
+++ 1428-jump-game-iii/1428-jump-game-iii.java
@@ -1,9 +1,18 @@
 class Solution {
     public boolean canReach(int[] arr, int start) {
         
+       return solve1(arr,start);
+    }
+
+    private boolean solve1(int[] arr, int start){
+
+        // Applying bfs
+        // TC : O(N)
+        // SC : O(2N)
+
         Queue<Integer> q = new LinkedList<>();
         int n  = arr.length;
-        boolean[] visit = new boolean[arr.length];
+        boolean[] visit = new boolean[arr.length];  // so that i won't visit the same index again
 
         q.offer(start);
 
@@ -11,11 +20,12 @@ class Solution {
 
             int curr = q.poll();
 
-            if(arr[curr] == 0) return true;
-
             int plus = curr + arr[curr];
 
-            if(plus>=0 && plus< n && !visit[plus]){ 
+            if(plus>=0 && plus< n && !visit[plus]){
+
+                if(arr[plus] == 0) return true;
+
                 q.offer(plus);
                 visit[plus] = true;
             }
@@ -23,6 +33,9 @@ class Solution {
             int minus = curr - arr[curr];
 
             if(minus >=0 && minus < n && !visit[minus]){ 
+
+                if(arr[minus] == 0) return true;
+
                 q.offer(minus);
                 visit[minus] = true;
             }
