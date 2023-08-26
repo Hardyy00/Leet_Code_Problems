dafdbb7c865911796ba4dfe68f895ec211f3adda
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2023-08-26 07:22:31 +0530

    Time: 61 ms (84.99%), Space: 65.7 MB (63.2%) - LeetHub

--- 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph.java
+++ 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph.java
@@ -1,9 +1,14 @@
 class Solution {
     public List<List<Integer>> getAncestors(int n, int[][] edges) {
 
+        // Using topo sort , to make connection from ancestors to descendents
+        // TC : O(N) + O(E) + O(N) + (N + E*N) + O(N^2)
+        // SC : O(N + E) + O(N) + O(N^2)
+
+        
         List<List<Integer>> adj = new ArrayList<>();
 
-        boolean[][] mat = new boolean[n][n];
+        boolean[][] mat = new boolean[n][n];  // adjacency matrix, to mark which node is an ancestor of which node
         int[] indegree = new int[n];
 
         for(int i=0;i<n;i++) adj.add(new ArrayList<>());
@@ -11,7 +16,7 @@ class Solution {
         for(int[] row : edges){
 
             adj.get(row[0]).add(row[1]);
-            mat[row[0]][row[1]] = true;
+            mat[row[0]][row[1]] = true; // row[0] is connected to 1
 
             indegree[row[1]]++;
         }
@@ -31,13 +36,17 @@ class Solution {
 
                 indegree[next]--;
 
+                // if a connected to b , then all the  nodes connected to a  must
+                // also be connected to b
+
+                // if mat[i][node] , i.e i is connected to node ,then i is also connected to next
                 for(int i=0;i<n;i++) if( mat[i][node] ) mat[i][next] = true;
 
                 if(indegree[next]==0) q.offer(next);
             }
         }
 
-
+        // we have made all the connections
         List<List<Integer>> ans = new ArrayList<>();
 
         for(int i=0;i<n;i++) ans.add(new ArrayList<>());
@@ -46,7 +55,9 @@ class Solution {
 
             for(int j=0;j<n;j++){
 
-
+                // if i is connected to j , then i is an ancestor of j
+                // since we are moving from 0 to n-1,we will always add a smaller ancestor 
+                // first (as we move from 0 to n-1, then order will be 0,1 2 ,3 ...)
                 if(mat[i][j]){
 
                     ans.get(j).add(i);
@@ -56,8 +67,5 @@ class Solution {
 
 
         return ans;
-
-
-        
     }
 }
