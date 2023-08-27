08b86b9f7eaef4acb80ccad6542997a77d997f67
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2023-08-27 12:21:20 +0530

    Time: 601 ms (21.91%), Space: 79.2 MB (22.23%) - LeetHub

--- 1706-min-cost-to-connect-all-points/1706-min-cost-to-connect-all-points.java
+++ 1706-min-cost-to-connect-all-points/1706-min-cost-to-connect-all-points.java
@@ -1,98 +1,61 @@
-class DisjointSet {
+class Pair{
 
-    private int[] parent;
+    int node;
+    int dis;
 
-    private int[] size;     
-    public DisjointSet(int n){
+    public Pair(int node, int dis){
 
-        this.parent = new int[n+1]; 
-        this.size = new int[n+1];
-
-        for(int i=0;i<=n;i++) parent[i] = i;
-
-        Arrays.fill(size, 1);      
+        this.node = node;
+        this.dis = dis;
     }
-
-    public int findParent(int node){
-
-        if(parent[node]==node) return node;
-
-        return parent[node] = findParent(parent[node]);
-    }
-
-    public void unionBySize(int u, int v){
-
-        int ultimateParentOfU = findParent(u);
-        int ultimateParentOfV = findParent(v);
-
-        if(ultimateParentOfU==ultimateParentOfV) return;
-
-        if(size[ultimateParentOfU] > size[ultimateParentOfV]){
-
-            parent[ultimateParentOfV] = ultimateParentOfU;      
-            size[ultimateParentOfU] += size[ultimateParentOfV];     
-
-        }else{      
-            parent[ultimateParentOfU] = ultimateParentOfV;      
-            size[ultimateParentOfV] += size[ultimateParentOfU];
-        }
-    }
-
 }
-
 class Solution {
     public int minCostConnectPoints(int[][] points) {
-        
+
         int n = points.length;
-        int size = n-1;
-        int tot =  (size * (size + 1))/2;
+        List<List<Pair>> adj = new ArrayList<>();
 
-        Integer[][] adj = new Integer[tot][3];
+        for(int i=0;i<n;i++) adj.add(new ArrayList<>());
 
-        int index =0;
         for(int i=0;i<n;i++){
 
-            for(int j=i+1; j<n;j++){
+            for(int j=0;j<n;j++){
 
                 int dis = Math.abs(points[j][0] - points[i][0]) + Math.abs(points[j][1] - points[i][1]);
-                
-                adj[index][0] = i;
-                adj[index][1] = j;
-                adj[index++][2] = dis;
+                adj.get(i).add(new Pair(j, dis));
             }
         }
 
-        
+        PriorityQueue<Pair> pq = new PriorityQueue<>((x,y) -> x.dis - y.dis);
 
+        boolean[] visit = new boolean[n];
 
-        Arrays.sort(adj, new Comparator<Integer[]>(){
+        pq.offer(new Pair(0,0));
+        int sum =0;
+        while(!pq.isEmpty()){
 
-            @Override
-            public int compare(Integer[] l1 , Integer[] l2){
+            int node = pq.peek().node;
+            int dis = pq.peek().dis;
+            
+            pq.poll();
 
-                return l1[2] - l2[2];
-            }
-        });
+            if(visit[node]) continue;
 
+            visit[node] = true;
+            sum += dis;
 
-        int sum = 0;
-        DisjointSet ds = new DisjointSet(n);
+            // System.out.println(node + " " + dis);
 
-        for(Integer[] row : adj){
+            for(Pair pair : adj.get(node)){
 
-            int u = row[0];
-            int v = row[1];
-            int w = row[2];
+                if(!visit[pair.node]){
 
-            if(ds.findParent(u) != ds.findParent(v)){
-
-                ds.unionBySize(u,v);
-                sum += w;
+                    pq.offer(pair);
+                }
             }
         }
 
         return sum;
-
         
     }
 }
