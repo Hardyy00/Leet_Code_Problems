2bc0fdd678d55dbbe7429c1187ee5ca01886c96d
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2023-08-29 07:46:54 +0530

    Time: 53 ms (62.11%), Space: 45.4 MB (89.44%) - LeetHub

---
+++ 2220-find-all-possible-recipes-from-given-supplies/2220-find-all-possible-recipes-from-given-supplies.java
@@ -0,0 +1,101 @@
+class Pair{
+
+    int index;
+    boolean isRecipe;
+
+    public Pair(int index, boolean isRecipe){
+
+        this.index = index;
+        this.isRecipe = isRecipe;
+    }
+
+    @Override
+    public String toString(){
+
+        return  "{ " +index + " " + isRecipe + " }";
+    }
+}
+class Solution {
+    public List<String> findAllRecipes(String[] recipes, List<List<String>> ingredients, String[] supplies) {
+
+        Map<String, Pair> map = new HashMap<>(); 
+        Queue<String> q = new LinkedList<>();
+
+        int index = 0;
+
+        for(String recipe : recipes){
+
+            map.put(recipe, new Pair(index++, true));
+        }
+        
+        for(int i=0;i<recipes.length;i++){
+
+            for(String s : ingredients.get(i)){
+
+                if(!map.containsKey(s)){
+
+                    map.put(s, new  Pair(index++, false));
+                }
+                
+            }
+        }
+        
+        
+
+        for(String s : supplies){
+
+            if(!map.containsKey(s)){
+
+                map.put(s, new  Pair(index++, false));
+            }
+
+            q.offer(s);
+        }
+
+        int m = map.size();
+
+        int[] indegree = new int[recipes.length];
+
+        List<List<String>> adj = new ArrayList<>();
+
+        for(int i=0;i<m;i++) adj.add(new ArrayList<>());
+
+        for(int i=0;i<recipes.length; i++){
+
+            String s = recipes[i];
+            indegree[i] = ingredients.get(i).size();
+
+            for(String it : ingredients.get(i)){
+
+                int in = map.get(it).index;
+                adj.get(in).add(s);
+            }
+        }
+
+        List<String> ans = new ArrayList<>();
+
+        while(!q.isEmpty()){
+
+            String s= q.poll();
+
+            Pair p = map.get(s);
+
+            int in = p.index;
+            boolean isRecipe= p.isRecipe;
+
+            if(isRecipe) ans.add(s);
+
+            for(String it : adj.get(in)){
+
+                int recIndex = map.get(it).index;
+
+                indegree[recIndex]--;
+
+                if(indegree[recIndex]==0) q.offer(it);
+                
+            }
+
+        }
+        return ans;
+    }
+}
