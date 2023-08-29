30e492d9432eef5f71d10a596b6944c69b861a6d
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2023-08-29 12:23:23 +0530

    Time: 16 ms (35.02%), Space: 44.4 MB (44.44%) - LeetHub

--- 2576-minimum-penalty-for-a-shop/2576-minimum-penalty-for-a-shop.java
+++ 2576-minimum-penalty-for-a-shop/2576-minimum-penalty-for-a-shop.java
@@ -6,19 +6,28 @@ class Solution {
 
     private int solve(String customers){
 
+        // Using a prefix sum and the suffix sum, to know what is the penalty if
+        // i close at ith day.
+
+        // TC : O(3N)
+        // SC : O(2N)
+
         int n = customers.length();
 
-        int[] futureVisit = new int[n+1];
+        int[] futureVisit = new int[n+1];  // O TO N RANGE
 
+        // calculate the penalty of future visit, if the shop is closed at ith day
         for(int i=n-1;i>=0;i--){
 
             char ch = customers.charAt(i);
-            int doesVisit = ch == 'Y' ? 1 : 0;
+            int doesVisit = ch == 'Y' ? 1 : 0;  // if current day a customer visit
 
             futureVisit[i] = doesVisit + futureVisit[i+1];
         }
 
-
+        int minIndex = 0;
+        
+        // in past if a customer visited or not
         int[] pastNotVisit = new int[n+1];
 
         for(int i=1;i<=n;i++){
@@ -27,18 +36,23 @@ class Solution {
             int doesNotVisit = ch =='N' ? 1 : 0;
 
             pastNotVisit[i] = doesNotVisit + pastNotVisit[i-1]; 
-        }
-
-        int minIndex = 0;
-
-        for(int i=0;i<=n;i++){
 
             if(futureVisit[i] + pastNotVisit[i] < futureVisit[minIndex] + pastNotVisit[minIndex]){
 
                 minIndex = i;
             }
-
         }
+
+        // int minIndex = 0;
+
+        // for(int i=0;i<=n;i++){
+
+        //     if(futureVisit[i] + pastNotVisit[i] < futureVisit[minIndex] + pastNotVisit[minIndex]){
+
+        //         minIndex = i;
+        //     }
+
+        // }
 
         return minIndex;
     }
