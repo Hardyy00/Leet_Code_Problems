fe44732e08d4b66a11a8c8de86e3ad9d39950202
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2023-09-05 07:59:13 +0530

    Time: 20 ms (45.16%), Space: 68.3 MB (70.16%) - LeetHub

--- 2379-maximum-total-importance-of-roads/2379-maximum-total-importance-of-roads.java
+++ 2379-maximum-total-importance-of-roads/2379-maximum-total-importance-of-roads.java
@@ -12,6 +12,64 @@ class Pair{
 class Solution {
     public long maximumImportance(int n, int[][] edges) {
 
+        // return solve1(n, edges);
+
+        return solve2(n, edges);
+        
+    }
+
+    public long solve2(int n, int[][] edges){
+
+        int[] temp = new int[n];
+
+        for(int[] row : edges){
+            temp[row[0]]++;
+            temp[row[1]]++;
+        }
+
+        List<Pair> list = new ArrayList<>();
+
+        for(int i=0;i<n;i++){
+            list.add(new Pair(i, temp[i]));
+        }
+
+        Collections.sort(list, new Comparator<>(){
+
+            @Override
+            public int compare(Pair p1, Pair p2){
+
+                return p1.indegree - p2.indegree;
+            }
+        });
+
+        int val = n;
+
+        for(int i=n-1;i>=0;i--){
+            Pair pair = list.remove(list.size()-1);
+            temp[pair.node] = val--;
+        }
+
+        long sum = 0;
+
+        for(int[] row : edges){
+
+            sum += temp[row[0]];
+            sum += temp[row[1]];
+        }
+
+        return sum;
+    }
+
+    public long solve1(int n, int[][] edges){
+
+
+        // Using Priority Queue
+        // The node with maximum indegree, occurs maximum times in the roads
+        // so on assigning it a big number we can make the sum bigger
+        // hence assigning number to nodes according to their indegree
+
+        // TC : O(M) + O(N) + O(N*Log N) + O(N*Log N) + O(N) + O(M)
+        // SC : O(N) + O(N) + O(N)  (indegree + pq + numbers)
         int[] indegree = new int[n];
 
         for(int[] row : edges){
@@ -34,7 +92,7 @@ class Solution {
 
             Pair pair = pq.poll();
 
-            numbers[pair.node] = n--;
+            numbers[pair.node] = val--;
 
         }
 
@@ -47,6 +105,5 @@ class Solution {
         }
 
         return sum;
-        
     }
 }
