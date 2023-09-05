74f21820512ceb1534321d3d2cf1d7be004195f6
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2023-09-05 14:44:39 +0530

    Time: 9 ms (98.62%), Space: 94.5 MB (89.42%) - LeetHub

--- 2403-count-unreachable-pairs-of-nodes-in-an-undirected-graph/2403-count-unreachable-pairs-of-nodes-in-an-undirected-graph.java
+++ 2403-count-unreachable-pairs-of-nodes-in-an-undirected-graph/2403-count-unreachable-pairs-of-nodes-in-an-undirected-graph.java
@@ -1,12 +1,99 @@
-class Solution {
-    public long countPairs(int n, int[][] edges) {
+class DisjointSet {
 
-       // getting the sizes of the components and multiplying each component
+    // path compression refer to connecting the node directly to the ultimate node, while returning from the recursion
+    public int[] parent;
+
+    public int[] size;     // using only one, (rank or size)
+
+    public DisjointSet(int n){
+
+        this.parent = new int[n+1]; // n+1 because, indexing might start from 1
+        this.size = new int[n+1];
+
+        for(int i=0;i<=n;i++) parent[i] = i;
+
+        Arrays.fill(size, 1);       // initially nodes are alone , so their size 1
+    }
+
+    public int findParent(int node){
+
+        // TC : O(4*a) == O(constant time) (a = alpha , a is very near to 1)
+
+        // if the parent of the node is the node itself , then he is the ultimate parent
+        if(parent[node]==node) return node;
+
+        // on returning apply path-Compression, connect the node, to the ultimate node
+        return parent[node] = findParent(parent[node]);
+    }
+
+    public void unionBySize(int u, int v){
+
+        // TC : O(4*a) == O(constant time) (a = alpha , a is very near to 1)
+
+        int ultimateParentOfU = findParent(u);
+        int ultimateParentOfV = findParent(v);
+
+        if(ultimateParentOfU==ultimateParentOfV) return;
+
+        if(size[ultimateParentOfU] > size[ultimateParentOfV]){
+
+            parent[ultimateParentOfV] = ultimateParentOfU;      // connect v to u
+            size[ultimateParentOfU] += size[ultimateParentOfV];     // as nodes are now connected to u, increase its size
+
+        }else{      // if the sizes are equal , then it doesn't , which nodes, we connect ,and we have to increase the size in both the cases
+
+            parent[ultimateParentOfU] = ultimateParentOfV;      // connect u to v
+            size[ultimateParentOfV] += size[ultimateParentOfU];
+        }
+    }
+}    
+
+
+class Solution {
+    public long countPairs(int n, int[][] edges) { 
+
+        // return solve1(n, edges);
+
+        return solve2(n, edges);
+       
+    }
+
+    private long solve2(int n, int[][] edges){
+
+        DisjointSet ds = new DisjointSet(n);
+
+        for(int row[] : edges){
+
+            int u = row[0];
+            int v = row[1];
+
+            ds.unionBySize(u,v);
+        }
+
+        long pairs =0 , rem = n;
+
+        for(int i=0;i<n;i++){
+
+            if(ds.findParent(i) == i){
+
+                long size = ds.size[i];
+                pairs += (rem - size) * size;
+
+                rem -= size;
+            }
+        }
+
+        return pairs;
+        
+    }
+
+    private long solve1(int n, int[][] edges){
+        // getting the sizes of the components and multiplying each component
        // size with every components , to get the number of pairs
        // the component can make with other components 
 
-       // TC : O(E) + O(N+2E) + O(C) , where c = number of components
-       // sc : O(N+2E) + O(N) + O(C)  
+       // TC : O(E) + O(N+2E)
+       // sc : O(N+2E) + O(N)
 
         List<List<Integer>> adj = new ArrayList<>();
 
@@ -30,14 +117,14 @@ class Solution {
                 dfs(i, visit, adj, counter);
 
                 pairs += (rem-counter[0])*(long)counter[0];
+                // the current components has x nodes, and the remaining node are n-x
+                // hence x nodes will make pair with n-x nodes
 
-                rem -= counter[0];
+                rem -= counter[0];  // deduct the current component nodes
             }
         }    
 
-        return pairs;    
-
-       
+        return pairs;   
     }
 
     private void dfs(int node, boolean[] visit, List<List<Integer>> adj, int[] counter){
@@ -54,4 +141,5 @@ class Solution {
         }
     }
 }
+
 
