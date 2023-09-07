864eb82f2b2614c62d0f14afa6b00e7aead94200
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2023-09-07 10:21:54 +0530

    Time: 9 ms (93.37%), Space: 120.7 MB (31.06%) - LeetHub

---
+++ 2582-minimum-score-of-a-path-between-two-cities/2582-minimum-score-of-a-path-between-two-cities.java
@@ -0,0 +1,74 @@
+class DisjointSet {
+
+    // path compression refer to connecting the node directly to the ultimate node, while returning from the recursion
+    private int[] parent;
+
+    private int[] size;     // using only one, (rank or size)
+
+    public DisjointSet(int n){
+
+        this.parent = new int[n+1]; // n+1 because, indexing might start from 1
+        this.size = new int[n+1];
+
+        for(int i=0;i<=n;i++) parent[i] = i;
+
+        Arrays.fill(size, 1);       // initially nodes are alone , so their size 1
+    }
+
+    public int findParent(int node){
+
+        // TC : O(4*a) == O(constant time) (a = alpha , a is very near to 1)
+
+        // if the parent of the node is the node itself , then he is the ultimate parent
+        if(parent[node]==node) return node;
+
+        // on returning apply path-Compression, connect the node, to the ultimate node
+        return parent[node] = findParent(parent[node]);
+    }
+
+    public void unionBySize(int u, int v){
+
+        // TC : O(4*a) == O(constant time) (a = alpha , a is very near to 1)
+
+        int ultimateParentOfU = findParent(u);
+        int ultimateParentOfV = findParent(v);
+
+        if(ultimateParentOfU==ultimateParentOfV) return;
+
+        if(size[ultimateParentOfU] > size[ultimateParentOfV]){
+
+            parent[ultimateParentOfV] = ultimateParentOfU;      // connect v to u
+            size[ultimateParentOfU] += size[ultimateParentOfV];     // as nodes are now connected to u, increase its size
+
+        }else{      // if the sizes are equal , then it doesn't , which nodes, we connect ,and we have to increase the size in both the cases
+
+            parent[ultimateParentOfU] = ultimateParentOfV;      // connect u to v
+            size[ultimateParentOfV] += size[ultimateParentOfU];
+        }
+    }
+
+}
+
+class Solution {
+    public int minScore(int n, int[][] roads) {
+
+       DisjointSet ds = new DisjointSet(n+1);
+
+       for(int[] row : roads){
+
+           ds.unionBySize(row[0], row[1]);
+       }
+
+       int minPath = Integer.MAX_VALUE;
+
+       for(int[] row : roads){
+
+           if(ds.findParent(row[0]) == ds.findParent(1) || ds.findParent(row[1]) == ds.findParent(1)){
+
+               minPath = Math.min(minPath, row[2]);
+           }
+       }
+
+       return minPath;
+    }
+}
