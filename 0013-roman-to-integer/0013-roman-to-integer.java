cf37c31db1a637ae1a921bddd6901a7462f8ccd0
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2024-06-11 07:27:43 +0530

    Time: 5 ms (57.14%), Space: 44.9 MB (20.64%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,60 +1,31 @@
 class Solution {
     public int romanToInt(String s) {
-
-        // use if else to check of the before condition if it satisfies then
-        // minus that particular value from the answer
-
-        // TC : O(N)
-        // SC : O(7)
         
-        Map<Character,Integer> map = new HashMap<>();
+        int n= s.length();
+        int ans= 0;
 
-        map.put('I',1);
-        map.put('V', 5);
-        map.put('X',10);
+        Map<Character, Integer> map = new HashMap<>();
+
+        map.put('I', 1);
+        map.put('V',5);
+        map.put('X', 10);
         map.put('L',50);
-        map.put('C',100);
-        map.put('D',500);
+        map.put('C', 100);
+        map.put('D', 500);
         map.put('M', 1000);
 
-        int ans = 0;
+        for(int i=0;i < n-1;i++){
 
-        int n= s.length();
+            char first = s.charAt(i), second = s.charAt(i+1);
 
-        for(int i=n-1;i>=0;i--){
-
-            char ch = s.charAt(i);
-            if(ch=='I' || ch=='X' || ch=='C'){
-
-                if(ch=='I'){
-
-                    if(i+1 < n && (s.charAt(i+1)=='V' || s.charAt(i+1)=='X')){
-                        ans -= map.get(ch);
-                    }else{
-                        ans += map.get(ch);
-                    }
-                }else if(ch=='X'){
-
-                    if(i+1 < n && (s.charAt(i+1)=='L' || s.charAt(i+1)=='C')){
-                        ans -= map.get(ch);
-                    }else{
-                        ans += map.get(ch);
-                    }
-
-                }else if(ch=='C'){
-
-                    if(i+1 < n && (s.charAt(i+1)=='D' || s.charAt(i+1)=='M')){
-                        ans -= map.get(ch);
-                    }else{
-                        ans += map.get(ch);
-                    }
-                }
-
+            if(map.get(first) < map.get(second)){
+                ans -= map.get(first);
             }else{
-
-                ans += map.get(ch);
+                ans += map.get(first);
             }
         }
+
+        ans += map.get( s.charAt(n-1) );
 
         return ans;
     }
