5eb8d825e6465204fe9eabefe494cdb5fa96ef15
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2024-06-25 11:49:36 +0530

    Time: 0 ms (100%), Space: 39.7 MB (98%) - LeetHub

--- 0233-number-of-digit-one/0233-number-of-digit-one.java
+++ 0233-number-of-digit-one/0233-number-of-digit-one.java
@@ -2,6 +2,16 @@ class Solution {
     int[][] dp = new int[12][2];
     int[][] countDp =new int[12][2];
     public int countDigitOne(int n) {
+
+        // using digit dp
+        // if i am putting 1 at a position , then count of number have 1, as their right digit
+        // will be count(n-1)
+
+        // hence dp(n,tight) = (dig==1 && count(n-1, tight)) + dp(n-1, (0 or 1))
+
+       // TC : O(logN)
+       // SC : O(logN)   
+
         for(int[] row : dp){
             Arrays.fill(row, -1);
         }
@@ -49,6 +59,7 @@ class Solution {
             return countDp[n][tight];
         }
 
+        // if tight ==0, then we can use 10 digits for each position , hence for n positions 10 ^9
         if(tight ==0){
             return (int)(Math.pow(10,n) + 0.1);
         }
@@ -57,14 +68,14 @@ class Solution {
             return 1;
         }
 
-
-
+        // if we must follow the tight constraint
         int upperBound = limit.charAt(limit.length()-n)-'0';
 
         int total = 0;
 
         for(int dig=0;dig<=upperBound;dig++){
-
+            
+            // send tight ==1 , if dig==upperBound
             total += count(limit,n-1, dig==upperBound ? 1 : 0);
         }
 
