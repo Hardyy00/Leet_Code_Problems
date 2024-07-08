fce12d677ff79bff9c88948dc7f38d01c484db1b
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2024-07-08 16:53:59 +0530

    Time: 4 ms (8.86%), Space: 43.8 MB (95.62%) - LeetHub

--- 0174-dungeon-game/0174-dungeon-game.java
+++ 0174-dungeon-game/0174-dungeon-game.java
@@ -4,14 +4,52 @@ class Solution {
     public int calculateMinimumHP(int[][] dungeon) {
 
 
-        for(int[] row : dp){
-            Arrays.fill(row, -1);
-        }
+        // for(int[] row : dp){
+        //     Arrays.fill(row, -1);
+        // }
         
         int n = dungeon.length , m = dungeon[0].length;
 
-        return memo(0,0,n,m, dungeon);
+        // return memo(0,0,n,m, dungeon);
+
+        return iterative(dungeon, n,m);
     }
+
+    private int iterative(int[][] mat, int n, int m){
+
+        int[][] dp = new int[n][m];
+
+        dp[n-1][m-1] = mat[n-1][m-1] < 0 ? 1 + Math.abs(mat[n-1][m-1]) : 1;
+
+        for(int row=n-1;row>=0;row--){
+
+            for(int col =m-1;col>=0;col--){
+
+                if(row==n-1 && col==m-1){
+                    continue;
+                }
+
+                int right = (int)1e9;
+
+                if(col + 1 < m){
+                    right = dp[row][col+1];
+                }
+
+                int down = (int)1e9;
+
+                if(row +1 < n){
+                    down = dp[row+1][col];
+                }
+
+                int heightFromHere = Math.min(right, down) - mat[row][col];
+
+                dp[row][col] = heightFromHere <=0 ? 1 : heightFromHere;
+            }
+        }
+
+        return dp[0][0];
+    }
+
 
     private int memo(int row, int col, int n, int m, int[][] mat){
 
