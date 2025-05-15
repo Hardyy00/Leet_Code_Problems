aa4c282132143a52855aac016edd66a11837f366
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2025-05-15 06:52:32 +0530

    Time: 0 ms (100%), Space: 42.4 MB (27.49%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,37 +1,120 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         
-        boolean doesExist = false;
-        
-        for(int i=0;i<matrix.length;i++){
-            
-            if(target>=matrix[i][0] && target<=matrix[i][matrix[0].length-1]){
-                return searchTarget(matrix[i] , target);
-            }
-        }
-        
-        return false;
-        
+        // return bruteForce(matrix, target);
+
+        // return betterSolution(matrix, target);
+
+        // return optimalSolution(matrix, target);
+
+        return  mostOptimalSolution(matrix, target);
     }
-    
-    private static boolean searchTarget(int[] arr , int target){
-        
-        int lower = 0;
-        int higher = arr.length-1;
-        
-        while(lower<=higher){
-            
-            int mid = lower + (higher-lower)/2;
-            
-            if(arr[mid] < target){
-                lower = mid+1;
-            }else if(arr[mid] > target){
-                higher = mid -1;
+
+    private boolean mostOptimalSolution(int[][] matrix, int target){
+
+        // Apply binary search on the whole matrix, with low = 0, and high = totalElements-1
+        // for the comparison , convert the mid to coordinates, row = mid / cols , col = mid % cols
+
+        // TC : O(log (m *n))
+        // SC : O(1)
+
+        int low = 0;
+        int high = (matrix.length * matrix[0].length) -1;
+
+
+        while(low <= high){
+
+            // int mid = (low + high)/2;
+
+            int mid = high + (low - high)/2;
+
+            int row = mid / matrix[0].length;
+            int col = mid % matrix[0].length;
+
+            if(matrix[row][col] < target){
+                low = mid+1;
+            }else if(matrix[row][col] > target){
+                high = mid-1;
             }else{
+
                 return true;
             }
         }
-        
+
+        return false;
+
+
+    }
+
+    private boolean optimalSolution(int[][] matrix, int target){
+
+        // Stand on the top right corner , and check if target is less than current element , if yes then
+        // move back in the row, cuz all elements behind are smaller, and if target is greater then move downwards,
+        // cuz all the element below are greater
+
+        // TC : O(M+N)
+        // SC : O(1)
+
+        int i=0;
+        int j=matrix[0].length-1;
+
+        while(j > - 1 && i < matrix.length){
+
+            if(matrix[i][j] == target) return true;
+            else if(target < matrix[i][j]) j--;
+            else if (target > matrix[i][j]) i++;
+        }
+ 
+        return false;
+    }
+
+    private boolean betterSolution(int[][] matrix, int target){
+
+        // Apply binary search on every row , since every row is sorted
+
+        // TC : O(m * logN)
+        // SC : O(1)
+
+        for(int i=0;i<matrix.length;i++){
+
+            if(binarySearch(matrix,i, target)) return true;
+        }
+
+        return false;
+    }
+
+    private boolean binarySearch(int[][] matrix, int row, int target){
+
+
+        int low = 0;
+        int high = matrix[0].length-1;
+
+        while(low <= high){
+
+            int mid = high + (low - high)/2;
+
+            if(matrix[row][mid] < target) low = mid+1;
+            else if(matrix[row][mid] > target) high = mid-1;
+            else return true; 
+        }
+
+        return false;
+    }
+
+    private boolean bruteForce(int[][] matrix, int target){
+
+        // Brute Force : Traverse the whole matrix 
+        // TC : O(m * n)
+        // SC : O(1)
+
+        for(int i=0;i<matrix.length;i++){
+
+            for(int j=0;j<matrix[0].length;j++){
+
+                if(matrix[i][j] == target) return true;
+            }
+        }
+
         return false;
     }
 }
