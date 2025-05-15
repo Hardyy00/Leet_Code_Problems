4619fb205fb963131b5a2566402213bbf5f328ab
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2025-05-15 08:04:13 +0530

    Time: 0 ms (100%), Space: 40.5 MB (61%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,82 +1,124 @@
 class Solution {
     public int uniquePaths(int m, int n) {
+        
+        // return uniquePathRecursion(m-1,n-1);
+
 
         // int[][] dp = new int[m][n];
-        // return moveOnGrid1(m-1,n-1,dp);
 
-        // return moveOnGrid2(m,n);
+        // for(int[] row  : dp) Arrays.fill(row,-1);
 
-        return moveOnGrid3(m,n);        
+        // return uniquePathMemo(m-1,n-1,dp);
+
+        // return uniquePathTab(m,n);
+
+        return uniquePathSpaceOpt(m,n);
     }
 
-    int moveOnGrid3(int m, int n){
+    private int uniquePathSpaceOpt(int m, int n){
 
-        // Space Optimization
-        // TC : O(M* N) && SC : O(N)
+        // Space optimisation
 
-        // only maintaining the row containing the previous answers to use it to calculate the upward direction paths
+        // TC : O(M*N)
+        // SC : O(N)
 
         int[] pre = new int[n];
-        int[] help = new int[n];
-        pre[0] = 1;
+
         for(int i=0;i<m;i++){
+
+            int[] curr = new int[n];
             for(int j=0;j<n;j++){
 
-                int up = pre[j];    // paths from upward direction
-                int left = j>0 ? help[j-1] : 0; // paths from left direction
-                help[j] = up + left;    // store the total paths from current cell
+                if(i==0 && j==0) curr[0] = 1;  // set the base case
+                else{
 
+                    int left = 0;
+                    int up = 0;
+
+                    if(j-1>=0) left = curr[j-1];
+
+                    if(i-1>=0) up = pre[j];
+
+
+                    curr[j] = left + up;
+                }
             }
 
-            // preserve the current answer as the previous answer
-            int[] swap = help;
-            help = pre;
-            pre = swap;
+            pre = curr;
         }
 
         return pre[n-1];
     }
 
-    int moveOnGrid2(int m, int n){
+    private int uniquePathTab(int m, int n){
 
-        // Tabulation 
-        // TC : O(M*N) && SC : O(M*N)
+        // Tabulation
+
+        // TC : O(N*M)
+        // SC : O(N*M)
 
         int[][] dp= new int[m][n];
+
         dp[0][0] = 1;
 
         for(int i=0;i<m;i++){
+
+
             for(int j=0;j<n;j++){
 
-                if(!(i==0 && j==0)){
-                    int left = 0;
-                    int up = 0;
+                if(i==0 && j==0) continue;  // base case is already set so skip it
 
-                    if(j-1>=0) left+= dp[i][j-1];
-                    if(i-1>=0) up += dp[i-1][j];
+                int left = 0;
+                int up =0;
 
-                    dp[i][j] = left + up;
-                }
+                if(j-1>=0) left = dp[i][j-1];
+                
+                if(i-1>=0) up = dp[i-1][j];
+
+                dp[i][j] = left + up;
             }
         }
 
-
         return dp[m-1][n-1];
+
+
+    }
+
+    private int uniquePathMemo(int r, int c, int[][] dp){
+        
+        // Memoization
+
+        // TC : O(N*M)
+        // SC : O(N*M) + O(N+M)
+
+        if(r==0 && c==0) return 1;
+
+        if(r < 0 || c < 0) return 0;
+
+        if(dp[r][c]!=-1) return dp[r][c];
+
+        int left = uniquePathMemo(r,c-1, dp);
+        int up = uniquePathMemo(r-1,c,dp);
+
+        return dp[r][c] = left + up;
     }
 
 
-    int moveOnGrid1(int m, int n,int[][] dp){
-        
-        // Memoization
-        // TC : O(M*N) && SC : O(M-1 + N-1) (path length due to recusion stack) + O(M*N)
 
-        if(m<0 || n < 0) return 0;
-        if(dp[m][n]!=0) return dp[m][n];
-        if(m==0 && n==0) return 1;
+    private int uniquePathRecursion(int r, int c){
 
-        int left = moveOnGrid1(m,n-1,dp);
-        int up = moveOnGrid1(m-1,n,dp);
+        // using recursion
 
-        return dp[m][n] = left + up;
+        // TC : O(exponential) or O(2^(m*n))
+        // sc : O(M+N) (explicit stack space)
+
+        if(r==0 && c==0) return 1;
+
+        if(r < 0 || c < 0) return 0;
+
+        int left = uniquePathRecursion(r,c-1);  //  move to top
+        int up = uniquePathRecursion(r-1,c);    // move to bottom
+
+        return left + up;
     }
 }
