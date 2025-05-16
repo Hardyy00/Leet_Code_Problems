42e64e030ffb17dd0e3ed2a493d3d95daf321e4d
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2025-05-16 07:21:48 +0530

    Time: 36 ms (24.77%), Space: 62.8 MB (62.58%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,36 +1,78 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
+
+        return optimalSolution(nums);
         
-        if(nums.length==0) return 0;
-        if(nums.length==1) return 1;
+    }
 
-        Set<Integer> set = new HashSet<>();
+    private int optimalSolution(int[] nums){
+
+        // Start iterating over the elements from the start of the sequence (start of sequence is when set does not
+        //  contains num-1) and on tarversing remove those elements, so we can skip over those in the future
+
+        // TC : O(2N)
+        // sc : O(N)
+
+        Set<Integer> set =new HashSet<>();
+
+        for(int i : nums) set.add(i);
+
+        int maxi = 0;
 
         for(int i : nums){
-            set.add(i);
-        }
 
-        int longestSeq = 0;
 
-        for(int i : nums){
-            
-            // its current element's previous element exists then its not the starting point
-            // of the consecutive sequence
+            if(!set.contains(i)) continue;  // element already traversed so skip over it
+
             if(!set.contains(i-1)){
 
-                int len =1;
-                // going through ont of the starting point of consecutive sequence
+                int count = 1;
+
+                set.remove(i);
+
                 while(set.contains(i+1)){
                     set.remove(i+1);
                     i++;
-                    len++; 
+                    count++;
                 }
 
-                longestSeq = Math.max(longestSeq,len);
+                maxi = Math.max(maxi, count);
             }
         }
 
-        return longestSeq;
+        return maxi;
     }
 
+    private int bruteForce(int[] nums){
+
+        // sort the elements first , and then check them,  for cases like this 0 1 1 2 2, skip over the equal elements
+        // to not include them in the sequence
+
+        // TC : O(N log N)
+        // SC : O(1)
+        if(nums.length==0) return 0;
+
+        Arrays.sort(nums);
+
+        int maxi = 0;
+        int count =1;
+
+        int n = nums.length;
+
+        for(int i=1;i<n;i++){
+            
+            // skip over equal elements
+            if(nums[i]==nums[i-1]) continue;
+            if(nums[i]==1+nums[i-1]) count++;
+            else{
+
+                maxi = Math.max(maxi,count);
+                count = 1;
+            }
+        }
+
+        maxi = Math.max(maxi,count);
+
+        return maxi;
+    }
 }
