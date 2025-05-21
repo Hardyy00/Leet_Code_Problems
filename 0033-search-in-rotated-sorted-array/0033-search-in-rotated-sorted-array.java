dc301ba31fda7bfac30911e4d14c60e24c8d2cdc
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2025-05-21 23:45:21 +0530

    Time: 0 ms (100%), Space: 41.9 MB (92.5%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,44 +1,41 @@
 class Solution {
     public int search(int[] nums, int target) {
-        
+
+        // SInce the array is sorted and then rotated so if seeing from middle a portion must
+        // be sorted, in that sorted portion check if the target can lie in that range
+        // if yes, then move to that place, else move to other space
+
+        // TC : O(logN)
+        // SC : O(1)
         
         int low = 0;
         int high = nums.length-1;
-        
+
         while(low<=high){
-            
-            int mid = high + (low - high)/2;
-            
+
+            int mid = high + (low-high)/2;
+
+
             if(nums[mid]==target) return mid;
-            
-            // check if left side is sorted
-            else if(nums[low]<=nums[mid]){
-                
-                // checking if the element exist within the left side
-                // if it exist then shifting the search space to left else to right
+
+            if(nums[low] <= nums[mid]){
+
                 if(target>=nums[low] && target<=nums[mid]){
-                    high = mid-1;
+                    high =mid-1;
                 }else{
-                    low = mid +1;
+                    low = mid+1;
                 }
-            }else{
-                
-                // for example like 6 7 1 2 3 4 5 . for finding 1 , 1<=2 || 1>=6 (number might be greater than 6)
-                // if(target>=nums[low] || target<=nums[mid])
-                
-                // checking if the element exist on the sorted right side
+            }else if(nums[mid]<=nums[high]){
+
                 if(target>=nums[mid] && target<=nums[high]){
                     low = mid+1;
+
                 }else{
-                    high = mid - 1;
+                    high = mid-1;
                 }
             }
-            
         }
-        
+
         return -1;
-        
-        
-        
     }
 }
