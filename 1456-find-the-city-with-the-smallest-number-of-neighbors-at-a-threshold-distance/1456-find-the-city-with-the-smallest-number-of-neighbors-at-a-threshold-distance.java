1e70cee9be07871c0c8631a7d293081a0387cd2f
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2025-06-22 22:37:43 +0530

    Time: 8 ms (90.6%), Space: 44.5 MB (51.19%) - LeetHub

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
@@ -1,10 +1,18 @@
 class Solution {
     public int findTheCity(int n, int[][] edges, int dt) {
+        
+        // applying floyd warshall algo to find distance of every node to every node
+        // and then finding which city has minimum number of city which we can be reach in
+        // threshold distance , if multiple cities exist , then just storing them 
+        // in a iterative way
 
+        // TC : O(N*N*N)
+        // SC : O(N^2)
         int[][] dis = new int[n][n];
 
         for(int[] row : dis) Arrays.fill(row , (int)1e8);
 
+        // make a adjacency matrix , edges are bi-directional
         for(int[] it : edges){
 
             dis[it[0]][it[1]] = it[2];
@@ -13,6 +21,7 @@ class Solution {
 
         for(int i=0;i<n;i++) dis[i][i] = 0;
 
+        // floyd warshall
         for(int via = 0; via < n; via++){
 
             for(int i=0;i<n;i++){
@@ -26,12 +35,12 @@ class Solution {
             }
         }
 
-        int minCount = Integer.MAX_VALUE;
-        int city = 0;
+        int minCount = Integer.MAX_VALUE;   // to track , minimum citites
+        int city = 0;   // initiall city can be anything
 
         for(int i=0;i<n;i++){
 
-            int count = 0;
+            int count = 0;  // count cities, with threshold distance
             for(int j=0;j<n;j++){
 
                 if(dis[i][j] <= dt){
@@ -40,6 +49,8 @@ class Solution {
                 }
             }
 
+            // minimize the cities count , and for the maximum condition, assign city 
+            // on every equal count, so the city value get increased
             if(count <= minCount){
                 minCount = count;
                 city = i;
