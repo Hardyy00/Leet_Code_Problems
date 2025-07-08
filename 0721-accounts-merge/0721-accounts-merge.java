1883e4ea906a8ba08abc0e9542998ea4774dec68
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2025-07-08 07:37:07 +0530

    Time: 28 ms (84.03%), Space: 47.5 MB (78.81%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.java
+++ 0721-accounts-merge/0721-accounts-merge.java
@@ -52,63 +52,78 @@ class DisjointSet {
 class Solution {
     public List<List<String>> accountsMerge(List<List<String>> accounts) {
         
+        // TC : O(N*M*Log M) : where m is the number of strings for each account
+        // TC : O(N*M * K *Log(N*M)) , tc for sortig n *m strings , n*m*k * Log(n*m), as there will be
+        // n *m *log(n*m) comparisions,and length of each comparision if k , on average
+
+        // SC : O(N*M) +O(N*M) + O(N*M)
 
         Map<String, Integer> map = new HashMap<>();
 
         int n = accounts.size();
-        DisjointSet ds = new DisjointSet(n);
+        DisjointSet ds = new DisjointSet(n);  // to make connection in case we find similar accounts
 
 
         for(int i=0;i<n;i++){
 
             for(int j=1;j<accounts.get(i).size();j++){
 
+                // if the mail is already in the map, that means someone must already have it
+                // in this case , this account too belongs to him, so make this node
+                // the child of the other node
                 if(map.containsKey(accounts.get(i).get(j))){
 
-                    int parent = map.get(accounts.get(i).get(j));
+                    // get parent through the map, as this string would be mapped to its original parent
+                    int parent = map.get(accounts.get(i).get(j));   
 
                     ds.unionBySize(parent, i);
                 }else{
 
+                    // otherwise, put this string into the map indicating , that the parent
+                    // of this mail is i
                     map.put(accounts.get(i).get(j), i);
                 }
             }
         }
 
+        // to merge accounts , just mergeing all the email to their original accounts
         List<List<String>> list = new ArrayList<>();
-        for(int i=0;i<n;i++) list.add(new ArrayList<>());
+        for(int i=0;i<n;i++) list.add(new ArrayList<>());   // we don't know which index
+        // would be empty so , create all indexes
 
+        // iterate over all the strings and their parents in the map
         for(Map.Entry<String,Integer> it : map.entrySet()){
 
             String s = it.getKey();
             int parent = it.getValue();
 
-            int uParent= ds.findParent(parent);
+            int uParent= ds.findParent(parent); // get the ultimate parent of account
 
-            list.get(uParent).add(s);
+            list.get(uParent).add(s);   // the mail belongs to uparent, so add it
         }
 
         List<List<String>> ans = new ArrayList<>();
 
         for(int i=0; i<n;i++){
 
+            // this account has been merged, so it is empty, no need to consider it
             if(list.get(i).isEmpty()) continue;
 
 
-            Collections.sort(list.get(i));
+            Collections.sort(list.get(i));  // sort all the emails
 
-            String name = accounts.get(i).get(0);
+            String name = accounts.get(i).get(0);   // get the name of the account holder
 
-            List<String> ls = new ArrayList<>();
+            List<String> ls = new ArrayList<>();    // to contain a account
 
             ls.add(name);
 
-            for(String s : list.get(i)){
+            for(String s : list.get(i)){    // add all the emails
 
                 ls.add(s);
             }
 
-            ans.add(ls);
+            ans.add(ls);    // add account
 
         }
 
